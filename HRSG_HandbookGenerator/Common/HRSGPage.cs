ffe6c3e550565ad2111fe9e080c80aa29e21ee0d
Shawn <sfreeman@nextlogik.com>
2016-01-12 09:48:20 -0500

    gitignore and and HRSG Page checkin

---
+++ HRSG_HandbookGenerator/Common/HRSGPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRSG_HandbookGenerator.Common {
+    public class HRSGPage : System.Web.UI.Page
+    {
+        protected static int? CurrentUserID { get; set; }
+
+        protected virtual void Page_Load(object sender, EventArgs e)
+        {
+            if (!CurrentUserID.HasValue)
+            {
+                if (Title.Contains("Log In")) return;
+                Response.Redirect("~/Account/Login.aspx", false);
+            }
+            else
+            {
+                if (Title.Contains("Log In")) Response.Redirect("~/Dashboard.aspx", false); ;
+                
+            }
+        }
+    }
+}
