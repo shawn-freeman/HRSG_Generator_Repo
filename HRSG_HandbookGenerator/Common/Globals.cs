ddaf87c48242e6185ba6cf857ee07ff7724cea15
Shawn <sfreeman@nextlogik.com>
2016-01-12 15:09:35 -0500

    Added Head Content and removed register link

---
+++ HRSG_HandbookGenerator/Common/Globals.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRSG_Datalayer;
+
+namespace HRSG_HandbookGenerator.Common {
+    public class Globals
+    {
+        #region Singleton Logic
+
+        private static bool _initialized = false;
+
+        private static Globals _instance;
+        public static Globals Instance {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Globals();
+                    _initialized = true;
+                }
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        public List<Industry> Industries { get; set; }
+
+        public List<EmployeeRanx> EmployeeRanges { get; set; } 
+
+        #endregion
+
+        private Globals()
+        {
+            if (!_initialized)
+            {
+                using (var hrsgEntities = new HRSG_DatabaseEntities())
+                {
+                    //store all the activate industries
+                    Industries = hrsgEntities.Industries.Where(i => i.Active).ToList();
+                    EmployeeRanges = hrsgEntities.EmployeeRanges.Where(e => e.Active).ToList();
+                }
+            }
+        }
+    }
+}
---
+++ HRSG_HandbookGenerator/Objects/ClientListingItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRSG_HandbookGenerator.Objects {
+    class ClientListingItem {
+        public int ID { get; set; }
+        public System.DateTime Modified { get; set; }
+        public System.DateTime Created { get; set; }
+        public string Name { get; set; }
+        public int? IndustryID { get; set; }
+        public string IndustryName { get; set; }
+        public string EmployeeRange { get; set; }
+    }
+}
